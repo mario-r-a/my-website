bcc189011689fc2dc0307c7becbde002dda68228
Mario Ruby A <pius.marioruby@gmail.com>
2025-05-17 17:24:14 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+// MARIO RUBY ARIESUSANDI / NIM 0706012410028 / CLASS WEB PROGRAMMING B
+
+$(document).ready(function () {
+    // Scroll to top when clicking logo-link, only if scrolled down
+    $('header nav a.logo-link').on('click', function (e) {
+        e.preventDefault();
+
+        if ($(window).scrollTop() > 100) {
+            $('html, body').animate({ scrollTop: 0 }, 600);
+        }
+        // else do nothing (stay at top)
+    });
+
+    // Toggle mobile menu open/close
+    $('#menu-toggle').on('click', function () {
+        $('#mobile-menu').toggleClass('translate-x-0 translate-x-full');
+        $('#menu-toggle-icon').toggleClass('hidden');
+        $('#menu-close-icon').toggleClass('hidden');
+    });
+
+    // Close mobile menu when clicking any link inside it
+    $('#mobile-menu a').on('click', function () {
+        $('#mobile-menu').removeClass('translate-x-0').addClass('translate-x-full');
+        $('#menu-toggle-icon').removeClass('hidden');
+        $('#menu-close-icon').addClass('hidden');
+    });
+
+    // Sections to highlight in nav
+    const sections = ['#hi', '#projects', '#skills'];
+
+    function activateNav() {
+        const scrollPos = $(document).scrollTop();
+
+        sections.forEach(id => {
+            const section = $(id);
+            if (section.length) {
+                const offset = section.offset().top - 100;
+                const height = section.outerHeight();
+
+                if (scrollPos >= offset && scrollPos < offset + height) {
+                    $('.fixed a').removeClass('active');
+                    $(`.fixed a[href="${id}"]`).addClass('active');
+                }
+            }
+        });
+    }
+
+    activateNav();
+    $(document).on('scroll', activateNav);
+});
